a292a8df45577118cd44f81acbe7cb6a24fa104a
Anaporka <anaporkaadazabra@gmail.com>
2020-10-06 03:54:57 +0000

    Updates

---
+++ precache-manifest.4c4743b1040d09f1ee935fc8190b22b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5b9fc28548827ee7bc924e471e8808",
+    "url": "/React-app-components/index.html"
+  },
+  {
+    "revision": "4af43ac0d9d35fe3e85d",
+    "url": "/React-app-components/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/React-app-components/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-components/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af43ac0d9d35fe3e85d",
+    "url": "/React-app-components/static/js/main.3f68ee8e.chunk.js"
+  },
+  {
+    "revision": "6ba5c3adfba2cb87debf",
+    "url": "/React-app-components/static/js/runtime-main.036aca5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-app-components/static/media/logo.5d5d9eef.svg"
+  }
+]);
